6adbf907b49c90205f9c9d636e31b5c9eb5b4050
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2019-05-08 18:08:51 -0600

    add: gallery menu

--- js/photoGallery/renderGallery.js
+++ js/photoGallery/renderGallery.js
@@ -1,22 +1,20 @@
 (function() {
-    const gallery = document.querySelector('#photoGallery div');
+  const menu = document.getElementById("sliderMenu");
 
-    function renderGallery(array) {
-        array.forEach((obj) => {
-            obj.sources.forEach((src, i) => {
-                const div = document.createElement('div');
-                const img = document.createElement('img');
-                const a = document.createElement('a');
-                a.innerText = obj.category;
-                a.href = src; 
-                a.target = '_blank';
-                img.src = src;
-                img.alt = `${obj.category} photo ${i + 1}`;
-                div.appendChild(img);
-                div.appendChild(a);
-                gallery.appendChild(div);
-            })
-        });
-    }
-    quicker().fetchJSON('data/photos.json', renderGallery);
-}());
+  function renderGallery(array) {
+    array.forEach((obj, i) => {
+      const div = document.createElement("div");
+      const littleDiv = document.createElement("div");
+      const img = document.createElement("img");
+      const h3 = document.createElement("h3");
+      img.src = obj.sources[i];
+      img.alt = `${obj.category} photo ${i + 1}`;
+      h3.innerText = obj.category;
+      littleDiv.appendChild(h3);
+      div.appendChild(img);
+      div.appendChild(littleDiv);
+      menu.appendChild(div);
+    });
+  }
+  quicker().fetchJSON("data/photos.json", renderGallery);
+})();
